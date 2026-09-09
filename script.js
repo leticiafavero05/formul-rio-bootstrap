d77331a320c25c2c412a87101aef0504850d6fe1
Leticia Favero <leticiafavero05@gmail.com>
2026-09-08 23:54:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('btnAjuda').addEventListener('click', function () {
+  alert('Preencha todos os campos marcados como obrigatórios (*).\n\nDica: o campo de telefone precisa estar no formato (11) 91234-5678, e a senha é apenas para demonstração, não use uma senha real.');
+});
